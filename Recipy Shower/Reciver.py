3aa40ba038908cbdd1d4b725f8c2e95141c0eec9
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-11-20 11:10:30 -0600

    vdxfdfsd

--- Recipy Shower/Reciver.py
+++ Recipy Shower/Reciver.py
@@ -4,6 +4,7 @@
 import socket               # Import socket module
 
 
+
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
@@ -14,5 +15,5 @@ s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print ('Got connection from', addr)
-   c.send(b'Thank you for connecting')   # Send bytes
+   c.send (b'Conected to')   # Send bytes
    c.close()                # Close the connection
