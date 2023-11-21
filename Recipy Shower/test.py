3805a6c00fb816fc4bf9a70f9c3360d7f0e67968
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-11-20 19:14:08 -0600

    dfgh

--- Recipy Shower/test.py
+++
@@ -1,7 +0,0 @@
-import urllib.request, sys 
-
-name = 'recipe'
-but = '3'
-
-
-urllib.request.urlretrieve('https://raw.githubusercontent.com/Blackbelt201911/Server/main/test2.txt', name + but + '.txt')
