025196db74785d0fd29186232a0afcea2cebf18c
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-11-22 20:26:38 -0600

    urytrury

--- Recipy Shower/Tk stuff.py
+++ Recipy Shower/Tk stuff.py
@@ -1,33 +1,44 @@
 from tkinter import *
-import tkinter as tk
+from ttkbootstrap import *
 import ttkbootstrap as tb
 
 
 def PostFunc():
-    inp = PostText.get(1.0, "end-1c")
-    print(inp)
+    global PostingText
+    PostingText = PostText.get(1.0, "end-1c")
+    print(PostingText)
 
+    RecpieLable = Label(TabInRecipeTab, text = PostingText, font =('Courier', 24))  
+    RecpieLable.pack()  
 
 
 root = tb.Window(themename="darkly")
 
 #root = Tk()
 root.title("Just like Mama's")
-root.geometry('720x500')
+root.geometry('750x500')
 
-Screens = tb.Notebook(root, bootstyle="dark")
+Screens = tb.Notebook(root, bootstyle="dark", width=675, height=424)
 Screens.pack(pady=20)
 
 PostTab = tb.Frame(Screens)
-Recipies = tb.Frame(Screens)
+RecipieTab = tb.Frame(Screens)
+
+my_canvas = Canvas(RecipieTab)
+my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+my_scroll_bar = ttk.Scrollbar(RecipieTab, orient=VERTICAL, command=my_canvas.yview)
+my_scroll_bar.pack(side=RIGHT, fill=Y)
+
+my_canvas.configure(yscrollcommand=my_scroll_bar.set)
+my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+TabInRecipeTab = Frame(my_canvas, )
+
+my_canvas.create_window((0,0), window=TabInRecipeTab, anchor="nw")
 
 
-
-my_scroll = tb.Scrollbar(Recipies, orient='vertical', bootstyle="dark round")
-my_scroll.pack(side="right", fill="y")
-
-
-label1 = Label(PostTab, text="Enter your Recipies", font=("Helvetica", 18))
+label1 = Label(PostTab, text="Enter your Recipie", font=("Bookman Old Style", 18))
 label1.pack(pady=20)
 
 PostText = Text(PostTab, width=70, height=10)
@@ -38,13 +49,13 @@ PostBT = tb.Button(PostTab, text="Post", bootstyle="danger outline", command = P
 PostBT.pack(pady=20)
 
 
-my_label2 = Label(Recipies, text="Recipies", font=("Helvetica", 18))
-my_label2.pack(pady=20)
+my_label2 = Label(TabInRecipeTab, text="Recipies", font=("Bookman Old Style", 28))
+my_label2.pack(pady=20, padx=265)
+
 
 # Add our frames to the notebook
-Screens.add(PostTab, text="Poster")
-Screens.add(Recipies, text="Recipies")
-
+Screens.add(PostTab, text="PotTab")
+Screens.add(RecipieTab, text="RecipieTab")
 
 
 
