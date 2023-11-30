14b633b13f4000bf16f4a2b382cb9fc230088aa9
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2023-11-30 07:57:09 -0600

    iuuy

--- Recipy Shower/Untitled-1.py
+++ Recipy Shower/Untitled-1.py
@@ -1,9 +1,25 @@
+# Program to show various ways to read and
+# write data in a text file.
+
+file = open("myfile.txt","w") #creates file
+L = ["This is Lagos","This is Python","This is Fcc"]
 
 
 
-with open('myfile'+'.txt', 'w') as f:
-    f.write('This is my string')
-# read the file and print its contents
-with open('myfile.txt', 'r') as f:
-    contents = f.read()
-    print(contents)
+
+file = open("myfile.txt","r+") 
+print("Output of the Read function is ")
+print(file.read())
+print()
+  
+# The seek(n) takes the file handle to the nth
+# byte from the start.
+
+  
+file.seek(0)
+# readlines function
+print("Output of Readlines function is ") 
+print(file.readlines()) 
+print()
+file.close()
+
