309e4f87a301a0eb793dab3e66b126de8819c3dd
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2024-01-21 14:38:08 -0600

    ffffffffff

--- Mite You Die/src/main.cpp
+++ Mite You Die/src/main.cpp
@@ -1,79 +1,32 @@
-#include <iostream>
-#include <raylib.h>
-#include "math.h"
+#include "test.cpp"
+#include "raylib.h"
 
-void drawstuff()
+
+int main(void)
 {
+   
     const int screenWidth = 800;
     const int screenHeight = 450;
-    
-    InitWindow(screenWidth, screenHeight, "Mite you die?");
 
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    Texture2D sprite = LoadTexture("pixil-frame-0.png");
+    SetTargetFPS(60);              
 
-    Vector2 spritePosition = { static_cast<float>(screenWidth / 2 - sprite.width / 2),
-                            static_cast<float>(screenHeight / 2 - sprite.height / 2) };
-
-    Vector2 AIPOS = { static_cast<float>(screenWidth / 2 - sprite.width / 2),
-                            static_cast<float>(screenHeight / 2 - sprite.height / 2) };
-
-    Vector2 objectCenter = { AIPOS.x, AIPOS.y }; 
-
-
-    int numRays = 36; 
-    float angleIncrement = 360.0f / numRays;
-
-    SetTargetFPS(60);               
-
+    // Main game loop
     while (!WindowShouldClose())    
     {
-       
-        if (IsKeyDown(KEY_D)) {
-            spritePosition.x += 2.5f;
-        }
-        if (IsKeyDown(KEY_A)) {
-            spritePosition.x -= 2.5f;
-        }
-        if (IsKeyDown(KEY_S)) {
-            spritePosition.y += 2.5f;
-        }
-        if (IsKeyDown(KEY_W)) {
-            spritePosition.y -= 2.5f;
-        }
 
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawTextureV(sprite, spritePosition, WHITE);
-
-            DrawTextureV(sprite, AIPOS, WHITE);
-
-
-            for (int i = 0; i < numRays; i++)
-            {
-                float angle = DEG2RAD * i * angleIncrement;
-                float rayX = objectCenter.x + 100 * cos(angle);
-                float rayY = objectCenter.y + 100 * sin(angle);
-
-                DrawLine(objectCenter.x, objectCenter.y, rayX, rayY, RED);
-            }
-
-
-            
+            pain();
 
         EndDrawing();
-       
-    }
 
+    }
 
     CloseWindow();        
 
-}
-
-
-int main(){
-
-    drawstuff();
+    return 0;
 }
---
+++ Mite You Die/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "raylib.h"
+using namespace std;
+
+void pain()
+{
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+};
+
