c9b844c6b66b5e6fb78185c9d48596b293d84fed
Blackbelt201911 <143038649+Blackbelt201911@users.noreply.github.com>
2024-01-26 13:49:14 -0600

    m

--- Mite You Die/test.cpp
+++ Mite You Die/test.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "raylib.h"
 #include "stdio.h"
 using namespace std;
@@ -6,5 +5,29 @@ using namespace std;
 void player()
 {
     
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    
+    Texture2D sprite = LoadTexture("pixil-frame-0.png");
+    
+    Vector2 spritePosition = { static_cast<float>(screenWidth / 2 - sprite.width / 2),
+                               static_cast<float>(screenHeight / 2 - sprite.height / 2) };
+
+    DrawTextureV(sprite, spritePosition, WHITE);
+    
+    if (IsKeyDown(KEY_D)) {
+            spritePosition.x += 2.5f;
+        }
+        if (IsKeyDown(KEY_A)) {
+            spritePosition.x -= 2.5f;
+        }
+        if (IsKeyDown(KEY_S)) {
+            spritePosition.y += 2.5f;
+        }
+        if (IsKeyDown(KEY_W)) {
+            spritePosition.y -= 2.5f;
+        }
+
+    DrawTextureV(sprite, spritePosition, WHITE);
 }
 
